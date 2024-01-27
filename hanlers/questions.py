1415da2065bcb56053e9d75778b868af4a8769d8
den wayde <den.wayde@yandex.ru>
2024-01-27 15:32:46 +0500

    +

--- hanlers/questions.py
+++ hanlers/questions.py
@@ -17,11 +17,19 @@ router = Router()  # [1]
 @router.message(Command("start"), StateFilter(None))  # [2]
 async def cmd_start(message: Message, state: FSMContext):
     await message.answer(
-        "Здравствуйте, Вас приветсвует бот, который поможет Вам забронировать место в зале. Давайте приступим.\nНапишите пожалуйста боту ФИО",
-        reply_markup=get_places()
+        "Здравствуйте, Вас приветсвует бот, который поможет Вам забронировать место в зале. Давайте приступим.\nНапишите пожалуйста боту ФИО"
     )
     await state.set_state(SetConfigsToBot.set_name)
 
+@router.message(SetConfigsToBot.set_name)
+async def name_proccessor(message: Message, state: FSMContext):
+    await state.update_data(name = message.text)
+    await bot.send_photo()
+    await message.answer(
+        "Приятно познакомиться. Выберите пожалуйста доступную область зала",
+        reply_markup = get_places()
+        )
+    
 
 
 # @router.message(F.text.lower() == "да")
